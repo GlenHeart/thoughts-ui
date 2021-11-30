f02819fecb043aa86c9a826824a03f5765897e4a
dairan <dairan103@aliyun.com>
2021-11-30 14:00:24 +0800

    chore: eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,103 @@ module.exports = {
   env: {
     browser: true,
     jest: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/prettier',
     '@vue/typescript/recommended',
-    '@vue/prettier/@typescript-eslint',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   rules: {
+    // js/ts
+    'no-console': ['warn', { allow: ['error'] }],
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    camelcase: ['error', { properties: 'never' }],
+
+    'no-var': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-void': 'error',
+    'prefer-const': [
+      'warn',
+      { destructuring: 'all', ignoreReadBeforeAssign: true }
+    ],
+    'prefer-template': 'error',
+    'object-shorthand': [
+      'error',
+      'always',
+      { ignoreConstructors: false, avoidQuotes: true }
+    ],
+    'block-scoped-var': 'error',
+    'no-constant-condition': ['error', { checkLoops: false }],
+
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'semi-style': ['off', 'never'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { disallowTypeAnnotations: false }
+    ],
+
+    // vue
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-model-argument': 'off',
+
+    // prettier
+    'prettier/prettier': 'error',
+
+    // import
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type'
+        ],
+
+        pathGroups: [
+          {
+            pattern: 'vue',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@vue/**',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@element-plus/**',
+            group: 'internal'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['type']
+      }
+    ],
+    'semi-style': ['off', 'never']
   },
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/dist/**', 'docs/*'],
       env: {
-        jest: true,
-      },
-    },
-  ],
-};
+        jest: true
+      }
+    }
+  ]
+}
