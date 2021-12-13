03c72664f0fc80a8ca872b7a8c772885c95df6d8
dairan <dairan103@aliyun.com>
2021-12-13 15:02:04 +0800

    暂存

--- packages/virtual-scroll/index.ts
+++ packages/virtual-scroll/index.ts
@@ -1,10 +1,6 @@
-import { IWithInstall } from '../../typings/global'
 import VirtualScroll from './src/virtual-scroll.vue'
 import { App } from 'vue'
 
 VirtualScroll.install = function (app: App): void {
   app.component(VirtualScroll.name, VirtualScroll)
 }
-
-const _VirtualScroll: IWithInstall<typeof VirtualScroll> = VirtualScroll
-export default _VirtualScroll
