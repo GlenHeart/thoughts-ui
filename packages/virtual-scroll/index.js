17aec27cf0fab801fef1dc9895ee885fbb212032
dairan <dairan103@aliyun.com>
2021-12-13 14:48:12 +0800

    暂存

--- packages/virtual-scroll/index.js
+++
@@ -1,7 +0,0 @@
-import VirtualScroll from './src/virtual-scroll.vue';
-VirtualScroll.install = function (app) {
-    app.component(VirtualScroll.name, VirtualScroll);
-};
-const _VirtualScroll = VirtualScroll;
-export default _VirtualScroll;
-//# sourceMappingURL=index.js.map
