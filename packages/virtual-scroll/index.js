ae68641850b52c985524b471b5fab7ee65f86f41
dairan <dairan103@aliyun.com>
2021-12-13 14:44:46 +0800

    feat: tsc -d

---
+++ packages/virtual-scroll/index.js
@@ -0,0 +1,7 @@
+import VirtualScroll from './src/virtual-scroll.vue';
+VirtualScroll.install = function (app) {
+    app.component(VirtualScroll.name, VirtualScroll);
+};
+const _VirtualScroll = VirtualScroll;
+export default _VirtualScroll;
+//# sourceMappingURL=index.js.map
