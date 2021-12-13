a78816ec6639c6de04fb4f0904423259b184c53c
dairan <dairan103@aliyun.com>
2021-12-13 17:42:52 +0800

    暂存

--- packages/virtual-scroll/src/virtual-scroll.vue
+++ packages/virtual-scroll/src/virtual-scroll.vue
@@ -1,40 +1,8 @@
-<script lang="tsx">
-import { defineComponent, toRefs, PropType } from 'vue'
-import VirtualScrollOption from './virtual-scroll-option.vue'
-import type { virtualScrollOption } from './type'
+<template>test</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
 export default defineComponent({
-  name: 'VirtualScroll',
-  components: {
-    VirtualScrollOption
-  },
-  props: {
-    options: {
-      type: Array as PropType<virtualScrollOption[]>,
-      default: () => []
-    }
-  },
-  setup(props, { emit }) {
-    const { options } = toRefs(props)
-    return () => (
-      <ul class="virtual-scroll">
-        {options.value.map((item) => (
-          <VirtualScrollOption
-            label={item.value}
-            itemHeight={32}
-          ></VirtualScrollOption>
-        ))}
-      </ul>
-    )
-  }
+  name: 'VirtualScroll'
 })
 </script>
-<style lang="scss">
-.virtual-scroll {
-  display: block;
-  height: 100%;
-  width: 100%;
-  min-width: 200px;
-  height: 200px;
-  overflow: auto;
-}
-</style>
